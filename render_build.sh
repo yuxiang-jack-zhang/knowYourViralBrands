c56a8a452d5da4d380ec63288534257762822123
Yuxiang (Jack) Zhang <161813265+yuxiang-jack-zhang@users.noreply.github.com>
2024-08-05 13:34:57 -0700

    Create render_build.sh

---
+++ render_build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install Playwright and its dependencies
+playwright install
+playwright install-deps
